2ff4e5b7d47938907c7d9191562c9ce60da324a7
Jinghui Hu <hujinghui@buaa.edu.cn>
2017-06-19 20:14:57 +0800

    remove toolbar menubar and blink cursor

--- init.el
+++ init.el
@@ -1,4 +1,21 @@
 (setq mac-command-modifier 'meta)
-(set-default-font "Menlo-16")
+
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/solarized")
-(load-theme 'solarized t)
+; (load-theme 'solarized t)
+
+; customize all
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(initial-frame-alist (quote ((width . 100) (height . 40))))
+ '(menu-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 160 :family "Menlo")))))
