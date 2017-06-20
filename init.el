5f4f11b21c57f18093404555e9ac89b7b3c894e5
Jinghui Hu <hujinghui@buaa.edu.cn>
2017-06-20 20:46:59 +0800

    add more config

--- init.el
+++ init.el
@@ -13,11 +13,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
- '(initial-frame-alist (quote ((width . 100) (height . 40))))
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(initial-frame-alist
+   (quote
+    ((width . 100)
+     (height . 30))))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (color-theme-sanityinc-solarized color-theme-solarized)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -25,4 +35,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 160 :family "Menlo")))))
+ '(default ((t (:height 150 :family "Monaco")))))
