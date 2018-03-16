c239de443b9821b172f2f6190c538096f8fc18f5
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-03-17 02:21:54 +0800

    always answer with 'y-or-p' instead of 'yes-or-no'

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -1,11 +1,13 @@
 ;; change meta key
 (setq mac-command-modifier 'meta)
 
-
+;; refuse backup on currect folder
 (make-directory "~/.emacs.d/autosaves/" t)
 (make-directory "~/.emacs.d/backups/" t)
-
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/" t)))
 (setq backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
+
+;; always use 'y or n', refuse 'yes of no'
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 (provide 'init-custom)
