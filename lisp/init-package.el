f88ef85968806ae16b9da43343780ba713b75e21
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-03-17 20:22:02 +0800

    添加elpa镜像地址的注释

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,5 +1,6 @@
 (require 'package)
 
+;; using tsinghua mirror as default source
 (setq package-archives
       '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
 	("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
