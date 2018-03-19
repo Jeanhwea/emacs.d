57c95bee8d4ea60f99fb014d45cea93cc9c3d94e
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-03-20 00:51:52 +0800

    重新组织添加一些初始化配置

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -1,7 +1,6 @@
-;; change meta key
-(setq mac-command-modifier 'meta)
-
+;; --------------------------------------------------------------------------
 ;; bind keys
+;; --------------------------------------------------------------------------
 (global-set-key (kbd "C-.") 'set-mark-command)
 (global-set-key (kbd "C-M-f") 'toggle-frame-fullscreen)
 
@@ -10,24 +9,40 @@
 ;(global-set-key (kbd "C-c r") 'recentf-open-files)
 ;(global-set-key (kbd "C-c v") 'evil-mode)
 
+;; --------------------------------------------------------------------------
+;; backup setting
+;; --------------------------------------------------------------------------
 ;; refuse backup on currect folder
 (make-directory "~/.emacs.d/autosaves/" t)
 (make-directory "~/.emacs.d/backups/" t)
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/" t)))
 (setq backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
 
+;; --------------------------------------------------------------------------
+;; more friendly interaction
+;; --------------------------------------------------------------------------
+;; change meta key
+(setq mac-command-modifier 'meta)
 ;; always use 'y or n', refuse 'yes of no'
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+
+;; --------------------------------------------------------------------------
+;; restore last configuration: files, workspace and so on
+;; --------------------------------------------------------------------------
 ;; recently opened files
 (recentf-mode 1)
-
 ;; save cursor position
 (save-place-mode 1)
+;; restore last opened files and config
+(desktop-save-mode 1)
 
+
+;; --------------------------------------------------------------------------
+;; editing setting
+;; --------------------------------------------------------------------------
 ;; auto complete () {} []
 (electric-pair-mode 1)
-
 ;; enable upcase/downcase
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
