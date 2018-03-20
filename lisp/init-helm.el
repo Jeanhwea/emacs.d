070177ae7afe1e4bfbfd62a5b6df624b7e0469a5
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-03-20 12:49:16 +0800

    adding C-x x for helm-M-x

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -3,5 +3,6 @@
 ;; override default key bindings
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "C-x r b") 'helm-bookmarks)
+(global-set-key (kbd "C-x x") 'helm-M-x)
 
 (provide 'init-helm)
