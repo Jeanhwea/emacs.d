235d8dcb06d8a44d5a0864e0f2530b7c153f54f1
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-05-15 09:46:21 +0800

    change set-mark-command to C-.

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -1,6 +1,6 @@
 ;; key bindings
-(global-set-key (kbd "C-.") 'set-mark-command)
-(global-set-key (kbd "C-x C-.") 'pop-global-mark)
+(global-set-key (kbd "C-,") 'set-mark-command)
+(global-set-key (kbd "C-x C-,") 'pop-global-mark)
 (global-set-key (kbd "C-j") 'join-line)
 
 
