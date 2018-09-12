1d0d429948d72b0283b87455c350bff6ff8ce904
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-09-12 10:55:10 +0800

    customize the javascript configuration

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -4,10 +4,21 @@
 (global-set-key (kbd "C-j") 'join-line)
 
 
-;; Tab, Space, Indentation setup
+;; Tab, Space, indentation setup
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq tab-width 4)
+
+;; Javascript indentation
+(add-hook 'js-mode-hook
+   (lambda()
+      (setq indent-tabs-mode nil)
+      (setq js-indent-level 2)
+      (setq tab-width 2)))
+
+
+;; Display trailing whitespace
+(setq-default show-trailing-whitespace t)
 
 
 (defun jh/temporary-buffer ()
