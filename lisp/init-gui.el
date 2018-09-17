c0d81c27852a658be812e52bc4113455c63d9f34
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-09-17 11:42:35 +0800

    seperate font setting from English and Chinese

--- lisp/init-gui.el
+++ lisp/init-gui.el
@@ -21,9 +21,15 @@
 ;; use bar as default cursor
 ;;(setq-default cursor-type 'bar)
 
-(when (string-equal "windows-nt" system-type)
-  (set-default-font "Courier New 14"))
 
+(when (string-equal "windows-nt" system-type)
+  (set-face-attribute
+    'default nil :font "Courier New 14")
+  ;; 中文字体单独设置
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font)
+      charset
+      (font-spec :family "Microsoft YaHei" :size 18))))
 
 
 (provide 'init-gui)
