f29cd128cf852dcba2f0673b02a0035a150f56b8
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-09-28 11:34:28 +0800

    get rid of "candidates are automatically lowercased" in company-mode

--- lisp/init-plugin.el
+++ lisp/init-plugin.el
@@ -3,6 +3,9 @@
 ;; -------------------------------------------------------------------------
 (when (require 'company)
   (add-hook 'after-init-hook 'global-company-mode)
+  ;; https://emacs.stackexchange.com/questions/10837/how-to-make-company-mode-be-case-sensitive-on-plain-text/10838
+  ;; https://github.com/company-mode/company-mode/issues/14
+  (setq company-dabbrev-downcase nil)
   ;; add normal keybindings
   (define-key company-mode-map (kbd "M-/") 'company-complete)
   (define-key company-active-map (kbd "M-/") 'company-other-backend)
