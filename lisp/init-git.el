458825b05f7b0bbfd868f01fbaa05092a8125cb6
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-05 09:14:15 +0800

    [add] magit repository directories

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,4 +1,13 @@
 (when (require 'magit)
+
+  ;; repositories for magit-list-repositories
+  (setq magit-repository-directories `((,user-emacs-directory . 0)))
+  (when (string-equal "darwin" system-type)
+    (add-to-list 'magit-repository-directories '("~/Codes" . 3)))
+  (when (string-equal "windows-nt" system-type)
+    (add-to-list 'magit-repository-directories '("e:/Codes" . 3)))
+
+  ;; keybinding
   (global-set-key (kbd "C-x g") 'magit-status))
 
 (provide 'init-git)
