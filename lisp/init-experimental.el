db9335d58931db701dd53c3fe9b8e8fb023fccd3
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-05 23:56:27 +0800

    [add] elfeed and some RSS source

--- lisp/init-experimental.el
+++ lisp/init-experimental.el
@@ -1,9 +1,29 @@
-;; -------------------------------------------------------------------------
+;; -----------------------------------------------------------------------------
 ;; browse-at-remote
-;; -------------------------------------------------------------------------
+;; -----------------------------------------------------------------------------
 ;; (when (require 'browse-at-remote)
 ;;   (defalias 'open 'browse-url))
 
+
+;; -----------------------------------------------------------------------------
+;; elfeed
+;; -----------------------------------------------------------------------------
+(when (require 'elfeed)
+  (setq elfeed-feeds
+    '(
+       ;; ----------------------------------------------------------------------
+       ;; emacs
+       ("http://planet.emacsen.org/atom.xml" emacsen emacs)
+       ("http://pragmaticemacs.com/feed/" pragmaticemacs emacs)
+       (" http://feeds.feedburner.com/XahsEmacsBlog" xah emacs)
+       ;; programming
+       ("http://www.ruanyifeng.com/feed.html" ruanyifeng blog coding)
+       ("http://blog.binchen.org/rss.xml" chenbin blog coding)
+       ("http://feeds2.feedburner.com/catonmat" catonmat blog coding)
+       ;; news
+       ("https://laod.cn/feed/" laod blog Internet)
+       ;; ----------------------------------------------------------------------
+       )))
 
 
 (provide 'init-experimental)
