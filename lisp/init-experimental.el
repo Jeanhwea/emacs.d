78a6ad8f74d90c5c86dfe3021f1616c95e192108
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-06 19:32:59 +0800

    [add] some defind-key binding for elfeed-search

--- lisp/init-experimental.el
+++ lisp/init-experimental.el
@@ -29,15 +29,51 @@
        ;; ----------------------------------------------------------------------
        ))
 
+  (defvar elfeed-search-default-filter "@18-months-ago #100 -junk +unread")
+
+  (defun jh/elfeed-search-reset-default-filter ()
+    "Reset elfeed filter to the default."
+    (interactive)
+    (setq elfeed-search-filter elfeed-search-default-filter)
+    (elfeed-search-update :force))
+  (defun jh/elfeed-mark-all-as-read ()
+    "Mark all entries read"
+    (interactive)
+    (mark-whole-buffer)
+    (elfeed-search-untag-all-unread))
+
+  (define-key elfeed-search-mode-map (kbd "S")
+    'jh/elfeed-search-reset-default-filter)
+  (define-key elfeed-search-mode-map (kbd "R")
+    'jh/elfeed-mark-all-as-read)
+
+  (defalias 'jh/elfeed-search-tag-all-important
+    (elfeed-expose #'elfeed-search-tag-all 'important)
+    "Add the `important' tag to all selected entries.")
+  (defalias 'jh/elfeed-search-untag-all-important
+    (elfeed-expose #'elfeed-search-untag-all 'important)
+    "Remove the `important' tag to all selected entries.")
+  (defalias 'jh/elfeed-search-tag-all-junk
+    (elfeed-expose #'elfeed-search-tag-all 'junk)
+    "Add the `junk' tag to all selected entries.")
+
+  (define-key elfeed-search-mode-map (kbd "i")
+    'jh/elfeed-search-tag-all-important)
+  (define-key elfeed-search-mode-map (kbd "I")
+    'jh/elfeed-search-untag-all-important)
+  (define-key elfeed-search-mode-map (kbd "j")
+    'jh/elfeed-search-tag-all-junk)
+
+
   ;; highlight some tags of feeds
   (defface good-elfeed-entry '((t :foreground "#39e")) "good feeds.")
-  (defface imp-elfeed-entry '((t :foreground "#f0a")) "important feeds.")
+  (defface important-elfeed-entry '((t :foreground "#f0a")) "important feeds.")
   (push '(good good-elfeed-entry) elfeed-search-face-alist)
-  (push '(imp imp-elfeed-entry) elfeed-search-face-alist)
+  (push '(important important-elfeed-entry) elfeed-search-face-alist)
 
   ;; display feeds last 18 months ago, total items not greater than 100, and
   ;; remove junk, show unread only
-  (setq-default elfeed-search-filter "@18-months-ago #100 -junk +unread")
+  (setq-default elfeed-search-filter elfeed-search-default-filter)
   (setq url-queue-timeout 30)
   (global-set-key (kbd "C-x w") 'elfeed))
 
