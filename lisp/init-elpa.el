102d93b838d6737bde23535e7308beb3fda5f05d
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-06 16:40:26 +0800

    [remove] some unused spaces

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -11,8 +11,8 @@
 (when (require 'package)
   ;; using tsinghua mirror as default source
   (setq package-archives
-    '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-	     ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+    '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+       ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 
   ;; must initialize package first
   (package-initialize))
