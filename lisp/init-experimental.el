25158238c75a3df035f5987b3a96632388fe6f7f
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-06 10:21:54 +0800

    [add] some rss feeds

--- lisp/init-experimental.el
+++ lisp/init-experimental.el
@@ -13,17 +13,30 @@
     '(
        ;; ----------------------------------------------------------------------
        ;; emacs
-       ("http://planet.emacsen.org/atom.xml" emacsen emacs)
+       ("http://planet.emacsen.org/atom.xml" emacsen emacs important)
        ("http://pragmaticemacs.com/feed/" pragmaticemacs emacs)
        (" http://feeds.feedburner.com/XahsEmacsBlog" xah emacs)
        ;; programming
-       ("http://www.ruanyifeng.com/feed.html" ruanyifeng blog coding)
+       ("http://www.ruanyifeng.com/blog/atom.xml" ruanyifeng blog coding)
        ("http://blog.binchen.org/rss.xml" chenbin blog coding)
        ("http://feeds2.feedburner.com/catonmat" catonmat blog coding)
+       ("https://nullprogram.com/feed/" nullprogram blog coding important)
+       ("https://blog.csdn.net/pennyliang/rss/list" pennyliang blog coding)
        ;; news
-       ("https://laod.cn/feed/" laod blog Internet)
+       ("https://laod.cn/feed/" laod blog news)
+       ("https://www.zhihu.com/rss" zhihu blog daily-popular news)
        ;; ----------------------------------------------------------------------
-       )))
+       ))
+
+  ;; highlight some tags of feeds
+  (defface good-elfeed-entry '((t :foreground "#39e")) "good feeds.")
+  (defface important-elfeed-entry '((t :foreground "#f0a")) "important feeds.")
+  (push '(good good-elfeed-entry) elfeed-search-face-alist)
+  (push '(important important-elfeed-entry) elfeed-search-face-alist)
+
+  (setq-default elfeed-search-filter "@2-months-ago -junk +unread")
+  (setq url-queue-timeout 30)
+  (global-set-key (kbd "C-x w") 'elfeed))
 
 
 (provide 'init-experimental)
