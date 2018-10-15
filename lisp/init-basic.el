1fbd5ca23a2d73e330b49361cc02b0e8a3c6a2a5
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-15 13:27:48 +0800

    [disable] windows shortcuts

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -8,11 +8,11 @@
 ;; -----------------------------------------------------------------------------
 ;; windows
 ;; -----------------------------------------------------------------------------
-(global-set-key (kbd "M-0") 'delete-window)
-(global-set-key (kbd "M-1") 'delete-other-windows)
-(global-set-key (kbd "M-2") 'split-window-below)
-(global-set-key (kbd "M-3") 'split-window-right)
-(global-set-key (kbd "M-o") 'other-window)
+;; (global-set-key (kbd "M-0") 'delete-window)
+;; (global-set-key (kbd "M-1") 'delete-other-windows)
+;; (global-set-key (kbd "M-2") 'split-window-below)
+;; (global-set-key (kbd "M-3") 'split-window-right)
+;; (global-set-key (kbd "M-o") 'other-window)
 
 (global-set-key (kbd "<f9>") 'make-frame-command)
 (global-set-key (kbd "<f10>") 'delete-frame)
