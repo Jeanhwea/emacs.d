07ca5028f006a3bb50afe47aa3cc1649be34baf0
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-21 18:30:40 +0800

    [add] projectile plugin

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -168,6 +168,15 @@
 (when (require 'multiple-cursors)
   (global-set-key (kbd "C-=") 'er/expand-region))
 
+;; -----------------------------------------------------------------------------
+;; projectile & helm-projectile
+;; -----------------------------------------------------------------------------
+(when (require 'projectile)
+  (projectile-mode 1)
+    (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+(when (require 'helm-projectile)
+  (global-set-key (kbd "C-c h") 'helm-projectile))
 
 ;; -----------------------------------------------------------------------------
 ;; tab, space width configuration
