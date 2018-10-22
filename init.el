5f3b7115f64afda9223c629a19468a2c8be7d3a3
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-22 19:22:22 +0800

    [add] htmlize package

--- init.el
+++ init.el
@@ -72,7 +72,7 @@
        ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
   '(package-selected-packages
      (quote
-       (helm-projectile projectile multiple-cursors rainbow-delimiters engine-mode elfeed nyan-mode fill-column-indicator emmet-mode tide markdown-mode js-comint expand-region nyan-mode company exec-path-from-shell helm evil browse-at-remote yasnippet smex magit color-theme-sanityinc-solarized ag))))
+       (htmlize helm-projectile projectile multiple-cursors rainbow-delimiters engine-mode elfeed nyan-mode fill-column-indicator emmet-mode tide markdown-mode js-comint expand-region nyan-mode company exec-path-from-shell helm evil browse-at-remote yasnippet smex magit color-theme-sanityinc-solarized ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
