24b07f81c5fbf19daff3c09f7282467a0feab8cd
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-23 08:51:44 +0800

    [change] yasnippet menu behavior

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,6 +1,9 @@
 (when (require 'yasnippet)
-  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
-  (setq yas-indent-line 'auto)
+  (setq
+    yas-snippet-dirs '("~/.emacs.d/snippets")
+    yas-visit-from-menu t
+    yas-indent-line 'auto)
+
   (yas-global-mode 1))
 
 (provide 'init-yasnippet)
