d36931fa3a3306cdf67d8e957a96fd8550b63e45
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-24 21:40:04 +0800

    [add] python-shell-interpreter on macOS

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -23,4 +23,9 @@
              ;; -----------------------------------------------------------------
              )))))
 
+(when (jh/mac?)
+  (setq
+    python-shell-interpreter "python3"
+    python-shell-interpreter-args "-i"))
+
 (provide 'init-python)
