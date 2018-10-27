5235581a928761b503820ea5feffcbe75b6948a2
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-27 22:31:34 +0800

    [change] plugin load order

--- init.el
+++ init.el
@@ -31,9 +31,9 @@
 ;; -----------------------------------------------------------------------------
 (require 'init-completion)
 (require 'init-elfeed)
+(require 'init-git)
 (require 'init-org)
 (require 'init-yasnippet)
-(require 'init-git)
 
 ;; -----------------------------------------------------------------------------
 ;; programming language
