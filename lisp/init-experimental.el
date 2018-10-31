336179efaecd6240a582d37ed687eeaefe777133
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-31 11:38:22 +0800

    [add] grip command

--- lisp/init-experimental.el
+++ lisp/init-experimental.el
@@ -43,13 +43,35 @@
 
 
 ;; -----------------------------------------------------------------------------
-;; clipboard-image
+;; grip
 ;; -----------------------------------------------------------------------------
 (defun jh/git-root-dir ()
-  "Get the root directory of a Git repository"
-  (expand-file-name
-    (shell-command-to-string
-      "git rev-parse --show-cdup")))
+  "Get the root directory of a Git repository."
+  (replace-regexp-in-string "\n" ""
+    (expand-file-name
+      (shell-command-to-string
+        "git rev-parse --show-cdup"))))
+
+(defvar grip-buffer "*grip*")
+
+(defun jh/grip (DIR FILENAME &optional HOST PORT)
+  "start a grip in backgroud in DIR."
+  (unless HOST (setq HOST "localhost"))
+  (unless PORT (setq PORT 2758))
+  (let ((default-directory DIR))
+    (when (get-buffer-process grip-buffer)
+      (quit-process grip-buffer))
+    (start-process "grip" grip-buffer "grip" "-b" FILENAME
+      (format "%s:%d" HOST PORT))))
+
+(defun grip ()
+	"start a grip daemon."
+	(interactive)
+	(let ((git-dir (jh/git-root-dir))
+         (filename
+           (replace-regexp-in-string (jh/git-root-dir) ""
+             (expand-file-name buffer-file-name))))
+    (when git-dir (jh/grip git-dir filename))))
 
 
 (require 'htmlize)
