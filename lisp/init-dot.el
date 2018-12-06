8e7fbf0f923437f59d1910145ec116f30b3d6636
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-12-06 08:25:12 +0800

    [add] hs-minor-mode to dot files

--- lisp/init-dot.el
+++ lisp/init-dot.el
@@ -4,6 +4,8 @@
       show-trailing-whitespace t
       indent-tabs-mode nil
       tab-width 2
-      graphviz-dot-indent-width 2)))
+      graphviz-dot-indent-width 2)
+    (hs-minor-mode 1)
+    (local-set-key (kbd "M-h") 'hs-toggle-hiding)))
 
 (provide 'init-dot)
