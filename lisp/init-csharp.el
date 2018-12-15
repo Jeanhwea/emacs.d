dde94546c3e6301c0c81a79509517787930ea22e
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-12-15 18:43:40 +0800

    [add] folding

--- lisp/init-csharp.el
+++ lisp/init-csharp.el
@@ -29,6 +29,10 @@
              )))
     ;; string-inflection
     (local-set-key (kbd "M-i") 'string-inflection-java-style-cycle)
-    (hl-line-mode 1)))
+    (hl-line-mode 1)
+    (rainbow-delimiters-mode 1)
+    (hs-minor-mode 1)
+    (local-set-key (kbd "C-c h") 'hs-toggle-hiding)))
+
 
 (provide 'init-csharp)
