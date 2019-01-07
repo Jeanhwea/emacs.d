e5d52945b38a29a28fe1df8fbbff4517166e2977
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-01-07 18:42:33 +0800

    [disable] dot graph 'graphviz-dot-auto-indent-on-semi'

--- lisp/init-dot.el
+++ lisp/init-dot.el
@@ -4,7 +4,10 @@
       show-trailing-whitespace t
       indent-tabs-mode nil
       tab-width 2
+      graphviz-dot-auto-indent-on-semi nil
       graphviz-dot-indent-width 2)
+    (hl-line-mode 1)
+    (rainbow-delimiters-mode 1)
     (hs-minor-mode 1)
     (local-set-key (kbd "C-c h") 'hs-toggle-hiding)))
 
