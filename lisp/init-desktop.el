df743ed7411de67bdcdadb2852f2b46018bc1f78
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-04-04 08:16:32 +0800

    add jh/* namespace

--- lisp/init-desktop.el
+++ lisp/init-desktop.el
@@ -92,13 +92,13 @@
     (after undo-tree activate)
     (setq ad-return-value (concat ad-return-value ".gz")))
   ;; Delete big file to avoid C stack overflow
-  (defun delete-big-undo-files ()
+  (defun jh/delete-big-undo-files ()
     (let ((max-undo-file-size 4096)
            (undodir (expand-file-name "undo" user-emacs-directory)))
       (dolist (undofile (directory-files undodir t "gz$"))
         (when (> (file-attribute-size (file-attributes undofile)) max-undo-file-size)
           (delete-file undofile)))))
-  (delete-big-undo-files)
+  (jh/delete-big-undo-files)
   (global-undo-tree-mode))
 
 
