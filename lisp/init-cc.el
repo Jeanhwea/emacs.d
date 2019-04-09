32900a5696ed5b0b523ec9eb2a06f63da633ec6a
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-04-09 16:49:26 +0800

    add some common setting for c++

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -1,3 +1,40 @@
+(add-hook 'c-mode-common-hook
+  (lambda()
+    (setq
+      show-trailing-whitespace t
+      indent-tabs-mode nil
+      c-basic-offset 2
+      tab-width 2
+      prettify-symbols-alist
+        (append prettify-symbols-alist
+          '(
+             ;; ----------------------------------------------------------------
+             ("public" . 9794)          ; ♂
+             ("private" . 9792)         ; ♀
+             ("this" . 964)             ; τ
+             ("return" . 1103)          ; я
+             ("new" . 1081)             ; й
+             ;; ----------------------------------------------------------------
+             ("null" . 1488)            ; א
+             ("NULL" . 1488)            ; א
+             ("true" . 8730)            ; √
+             ("false" . 215)            ; ×
+             ;; ----------------------------------------------------------------
+             ("&&" . 8743)              ; ∧
+             ("||" . 8744)              ; ∨
+             ("!" . 172)                ; ¬
+             ("<=" . 8804)              ; ≤
+             ("!=" . 8800)              ; ≠
+             ;; ----------------------------------------------------------------
+             )))
+    ;; string-inflection
+    (local-set-key (kbd "M-i") 'string-inflection-java-style-cycle)
+    (hl-line-mode 1)
+    (rainbow-delimiters-mode 1)
+    (highlight-indent-guides-mode 1)
+    (hs-minor-mode 1)
+    (local-set-key (kbd "C-c h") 'hs-toggle-hiding)))
+
 (when (require 'cuda-mode)
   (add-to-list 'auto-mode-alist '("\\.cu.cc\\'" . cuda-mode)))
 
