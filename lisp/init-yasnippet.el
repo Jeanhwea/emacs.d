d2f266a2d6a08f5cc7597e3a294110c84a00913d
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-05-11 21:01:58 +0800

    add import java class by class name

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -38,10 +38,10 @@
 ;; -----------------------------------------------------------------------------
 ;; helper function for yasnippets
 ;; -----------------------------------------------------------------------------
-(defun jh/file-name ()
+(defun jh/file-name (&optional file)
   "Return the file name without extension."
-  (file-name-nondirectory
-    (file-name-sans-extension (buffer-file-name))))
+  (setq file (or file (buffer-file-name)))
+  (file-name-nondirectory (file-name-sans-extension file)))
 
 (defun jh/parent-dir (file)
   "Return parent directory of the FILE."
@@ -56,7 +56,18 @@
     (insert-file-contents file)
     (split-string (buffer-string) "\n" t)))
 
-(defun jh/extract-package-class (line)
+(defun jh/java-project-source-files (&optional file)
+  "Return a list of `*.java' files in the project which contains the FILE."
+  (setq file (or file (buffer-file-name)))
+  (let ((project-src-dir
+          (when (string-match ".*src\\(/\\(main\\|test\\)\\)?\\(/java\\)?" file)
+            (expand-file-name
+              (replace-regexp-in-string
+                "/\\(main\\|test\\)/java/.*$" "" file)))))
+    (unless (null project-src-dir)
+      (directory-files-recursively project-src-dir "^.*\.java$"))))
+
+(defun jh/extract-java-package-class (line)
   "Extract package name and class name from line."
   (save-match-data
     (and (string-match "^import \\([^;]*\\)\\.\\([a-zA-Z0-9]*\\);$" line)
@@ -64,16 +75,28 @@
         class (match-string 2 line))
       (list class package))))
 
-(defun jh/read-imported-class (file)
+(defun jh/read-java-imported-class (file)
   "Read imported class in the FILES, then put them into a cache."
   (let ((class-cache (make-hash-table :test 'equal)))
-    (dolist (ele (mapcar #'jh/extract-package-class (jh/read-file-lines file)))
-      (unless (null ele) (puthash (car ele) (cadr ele) class-cache)))
+    (dolist (java-src-file (jh/java-project-source-files file))
+      (puthash (jh/java-class-name file) (jh/java-package-name file) class-cache)
+      (dolist (ele (mapcar #'jh/extract-java-package-class (jh/read-file-lines java-src-file)))
+        (unless (null ele) (puthash (car ele) (cadr ele) class-cache))))
     class-cache))
 
-;; (cadr '("a" "b"))
-;; (remove-if 'null (mapcar #'jh/extract-package-class (jh/read-file-lines "/Users/hujinghui/Code/work/avic/skree/src/main/java/com/avic/mti/skree/user/controller/EmployeeController.java")))
-;; (jh/read-imported-class "/Users/hujinghui/Code/work/avic/skree/src/main/java/com/avic/mti/skree/user/controller/EmployeeController.java")
+(defun jh/java-class-need-imported-p (clz &optional file)
+  "Return ture when the class is needed imported."
+  (setq file (or file (buffer-file-name)))
+  (let ((class-cache (make-hash-table :test 'equal)))
+    (dolist (ele (mapcar #'jh/extract-java-package-class (jh/read-file-lines file)))
+      (unless (null ele) (puthash (car ele) (cadr ele) class-cache)))
+    (null (gethash clz class-cache))))
+
+;; (remove-if 'null (mapcar #'jh/extract-java-package-class (jh/read-file-lines "/Users/hujinghui/Code/work/avic/skree/src/main/java/com/avic/mti/skree/user/controller/EmployeeController.java")))
+;; (gethash "Employee" (jh/read-java-imported-class "/Users/hujinghui/Code/work/avic/skree/src/main/java/com/avic/mti/skree/user/controller/EmployeeController.java"))
+;; (jh/java-class-need-imported-p "EmployeeType" "/Users/hujinghui/Code/work/avic/skree/src/main/java/com/avic/mti/skree/user/controller/EmployeeController.java")
+;; (jh/java-class-need-imported-p "List" "/Users/hujinghui/Code/work/avic/skree/src/main/java/com/avic/mti/skree/user/controller/EmployeeController.java")
+;; (jh/java-class-need-imported-p "Employee" "/Users/hujinghui/Code/work/avic/skree/src/main/java/com/avic/mti/skree/user/controller/EmployeeController.java")
 
 (defun jh/java-package-name (&optional file)
   "Return the package name for a java file."
@@ -84,10 +107,32 @@
         ".*src\\(/\\(main\\|test\\)\\)?\\(/java\\)?"
         "" dir) "/" t) "."))
 
-(defun jh/java-class-name ()
+(defun jh/java-class-name (&optional file)
   "Return the class name for java."
   (interactive)
-  (jh/pascalcase (jh/file-name)))
+  (jh/pascalcase (jh/file-name file)))
+
+(defun jh/java-import-package-statement (clz &optional file)
+  "Return the importing java package statement."
+  (setq file (or file (buffer-file-name)))
+  (let ((package (gethash clz (jh/read-java-imported-class file))))
+    (unless (null package) (format "import %s.%s;" package clz))))
+
+(defun jh/java-try-import-class (&optional clz)
+  "Try import class"
+  (interactive)
+  (let
+    ((stmt (jh/java-import-package-statement (or clz (word-at-point)))))
+    (save-excursion
+      (progn
+        (beginning-of-buffer)
+        (next-line)
+        (newline)
+        (insert (or stmt ""))))))
+(add-hook 'java-mode-hook (lambda () (local-set-key (kbd "M-RET") 'jh/java-try-import-class)))
+
+;; (jh/java-import-package-statement "EmployeeRepository" "/Users/hujinghui/Code/work/avic/skree/src/main/java/com/avic/mti/skree/user/controller/EmployeeController.java")
+;; (jh/java-import-package-statement "Logger" "/Users/hujinghui/Code/work/avic/skree/src/main/java/com/avic/mti/skree/user/controller/EmployeeController.java")
 
 (defun jh/java-test-case-name ()
   "Generate test case name with random time string."
