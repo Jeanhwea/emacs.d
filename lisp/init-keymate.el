ce7fe17b4457ef868565d31774e3279c6e003b43
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-06-25 10:35:30 +0800

    add km/M-x

--- lisp/init-keymate.el
+++ lisp/init-keymate.el
@@ -6,10 +6,16 @@
     ((eq major-mode 'python-mode) (elpy-format-code))
     (t (message "Ops, no format backend!"))))
 
+(defun km/M-x ()
+  "Start a command M-x with prefix `^jh/'"
+  (interactive)
+  (counsel-M-x "jh/"))
+
 (progn
   (define-prefix-command 'km/leader-key-map)
   (define-key km/leader-key-map (kbd "c") 'ct/expand-command)
-  (define-key km/leader-key-map (kbd "f") 'km/format-source-codes))
+  (define-key km/leader-key-map (kbd "f") 'km/format-source-codes)
+  (define-key km/leader-key-map (kbd "x") 'km/M-x))
 (global-set-key (kbd "M-k") 'km/leader-key-map)
 
 (provide 'init-keymate)
