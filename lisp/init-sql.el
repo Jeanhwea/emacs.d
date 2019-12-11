9ba52739197c03525601df3fc8a7966c7c49731d
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-12-11 18:22:25 +0800

    add some prefix on init-sql

--- lisp/init-sql.el
+++ lisp/init-sql.el
@@ -1,11 +1,11 @@
 (add-hook 'sql-mode-hook
-  (lambda()
-    (setq
-      show-trailing-whitespace t
-      indent-tabs-mode nil
-      tab-width 2)
-    (sqlind-minor-mode 1)
-    (hl-line-mode 1)))
+  #'(lambda()
+      (setq
+        show-trailing-whitespace t
+        indent-tabs-mode nil
+        tab-width 2)
+      (sqlind-minor-mode 1)
+      (hl-line-mode 1)))
 
 (when (jh/windows?)
   (setq sql-mysql-program "mysql")
