1ac42b468e41c4380993847d69cf2b0478141b3d
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-02-04 07:19:51 +0800

    update sql arguments

--- lisp/init-sql.el
+++ lisp/init-sql.el
@@ -7,7 +7,7 @@
       ;; for sqlformat
       (setq
         sqlformat-command 'sqlformat
-        sqlformat-args '("--keywords" "lower" "--indent_width" "2"))
+        sqlformat-args '("--keywords" "lower" "--indent_width" "2" "--reindent_aligned"))
       (sqlind-minor-mode 1)
       (hl-line-mode 1)))
 
