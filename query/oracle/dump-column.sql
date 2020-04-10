c9c2dff1123bfc42ad9f49ef14756dc370520575
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-04-10 21:56:02 +0800

    add dump column sql for oracle

---
+++ query/oracle/dump-column.sql
@@ -0,0 +1,49 @@
+select
+  t.ispk || ',' || t.isuniq || ',' || t.isnul || ',' || t.colname || ',' ||
+    t.coltype || ',' || t.collen || ',' || t.colpcs || ',' || t.colid
+from (
+  select
+    (
+      select
+        'p'
+      from
+        user_cons_columns t2,
+        user_constraints t3
+      where
+        t3.constraint_name = t2.constraint_name
+        and t1.column_name = t2.column_name
+        and t1.table_name = t2.table_name
+        and lower(t3.constraint_type) = 'p'
+        and rownum <= 1) as ispk, (
+        select
+          'u'
+        from
+          user_cons_columns t4,
+          user_constraints t5
+        where
+          t5.constraint_name = t4.constraint_name
+          and t1.column_name = t4.column_name
+          and t1.table_name = t4.table_name
+          and lower(t5.constraint_type) = 'u'
+        group by
+          t5.constraint_name
+        having
+          count(1) = 1) as isuniq,
+        decode(lower(t1.nullable), 'n', '*', '') as isnul,
+        t1.column_name as colname,
+        t1.data_type as coltype,
+        t1.data_length as collen,
+        t1.data_precision as colpcs,
+        (
+          select
+            replace(replace(t6.comments, chr(13), ''), chr(10), '\n')
+          from
+            user_col_comments t6
+          where
+            t6.column_name = t1.column_name
+            and t6.table_name = t1.table_name
+            and rownum <= 1) as colcmt, t1.column_id as colid
+        from
+          user_tab_columns t1
+        where
+          lower(t1.table_name) = lower('&tablename')) t;
