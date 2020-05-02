aa89090dd7685e279e51e70f9a34d71deaff3656
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-05-02 21:29:02 +0800

    fix

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -27,7 +27,7 @@
   (ivy-mode 1)
   (setq
     ivy-use-virtual-buffers t
-    ivy-height (floor (* (window-height) 0.85))
+    ivy-height (min (window-height) 30)
     enable-recursive-minibuffers t)
 
   (global-set-key (kbd "C-s") 'swiper)
