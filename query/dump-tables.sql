75573dad51e8a395d3a71ad012dd6e4a61e054ba
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-12-03 10:13:26 +0800

    Oracle 同时选取表名和视图名称

--- query/dump-tables.sql
+++ query/dump-tables.sql
@@ -2,12 +2,12 @@ select
   '&lpre' || t.tabname || '&fsep' || substr(t.tabcmt, 1, 40) as rowdata
 from (
   select
-    t1.table_name as tabname, --> Table Name
+    t1.object_name as tabname, --> Table Name
     replace(replace(t2.comments, chr(13), ''), chr(10), '&lsep') as tabcmt --> Table Comments
   from
-    user_tables t1,
+    user_objects t1,
     user_tab_comments t2
   where
-    t1.table_name = t2.table_name
+    t1.object_name = t2.table_name and lower(t1.object_type) in ('table', 'view')
   order by
     tabname) t;
