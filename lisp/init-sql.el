378dc5737c36b275a34d3c4f5ca58c80b5897134
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-04-19 14:31:53 +0800

    update

--- lisp/init-sql.el
+++ lisp/init-sql.el
@@ -23,16 +23,22 @@
   (expand-file-name "lang/oracle-func-name.txt" user-emacs-directory)
   "Location of pg_format extra function names.")
 
+;; http://sqlformat.darold.net/
+;; https://github.com/darold/pgFormatter
 (defconst pgformat-command
   (format
     "%s -p '\\?[0-9]+' -f 2 -u 2 -U 2 -s 2 -w 80 --extra-function \"%s\" -"
     pgformat-prog pgformat-func-dict)
-  "pg_format command on windows.")
+  "pg_format command.")
 
 ;; pip install sqlparse
 (defconst sqlformat-command
   "sqlformat - -k upper -i upper -s -a --indent_width 2 --wrap_after 80"
   "sqlformat command.")
+
+(defconst sql-format-command
+  (format "%s | %s" sqlformat-command pgformat-command)
+  "format sql command.")
 
 (defun jh/format-sql-source (&optional file)
   "Format sql source code."
@@ -45,12 +51,12 @@
       (when (> (point) (point-min)) (forward-char))
       (setq beg (point))
       (forward-paragraph)
-      (backward-char)
+      ;; (when (< (point) (point-max)) (backward-char))
       (setq end (point)))
     ;; execute commands
     (if (file-exists-p pgformat-name)
       (let ((default-directory (file-name-directory pgformat-name)))
-        (shell-command-on-region beg end pgformat-command nil t))
+        (shell-command-on-region beg end sql-format-command nil t))
       (shell-command-on-region beg end sqlformat-command nil t))
     ;; goto previous place
     (when (<= current-point (point-max)) (goto-char current-point))))
