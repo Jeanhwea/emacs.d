39c9c220ea4ed8305986a0ae787a2bdf2579a4a9
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-07-23 23:06:34 +0800

    fix: magit 打开远程 URL 的方法

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -11,48 +11,61 @@
 ;; browse-at-remote
 ;; -----------------------------------------------------------------------------
 (when (require 'browse-at-remote)
-  (add-to-list 'browse-at-remote-remote-type-domains '("192.168.0.202" . "gitlab2"))
-  (add-to-list 'browse-at-remote-remote-type-domains '("mtiisl.cn" . "gitlab3"))
-  (add-to-list 'browse-at-remote-remote-type-domains '("minix.jeanhwea.io" . "gitlab3"))
 
-  (defun browse-at-remote--format-commit-url-as-gitlab2 (repo-url commithash)
-    "commit url formatted for gitlab2."
-    (let
-      ((repo-url
-         (jh/re-replace "^https://" "http://" repo-url)))
-      (browse-at-remote--format-commit-url-as-gitlab repo-url commithash)))
+  (add-to-list 'browse-at-remote-remote-type-regexps
+    '("^mtiisl\\.cn/gitlab$" . "gitlab"))
 
-  (defun browse-at-remote--format-region-url-as-gitlab2
-    (repo-url location filename &optional linestart lineend)
-    "url formatted for gitlab2."
-    (let
-      ((repo-url
-         (jh/re-replace "^https://" "http://" repo-url)))
-      (browse-at-remote--format-region-url-as-gitlab
-        repo-url location filename linestart lineend)))
+  (defadvice browse-at-remote--get-url-from-remote
+    (after mtiisl-gitlab activate)
+    (let ((domain (car ad-return-value)) (url (cdr ad-return-value)))
+      (setq ad-return-value
+        (cons
+          (jh/re-replace "mtiisl.cn" "mtiisl.cn/gitlab" domain)
+          (jh/re-replace "mtiisl.cn" "mtiisl.cn/gitlab" url)))))
 
-  (defun browse-at-remote--format-commit-url-as-gitlab3 (repo-url commithash)
-    "commit url formatted for gitlab3."
-    (let
-      ((repo-url
-         (jh/re-replace
-           "^\\(https://\\)\\([^/]+\\)\\(.+\\)$"
-           "http://\\2/gitlab\\3" repo-url)))
-      (browse-at-remote--format-commit-url-as-gitlab repo-url commithash)))
+  ;; (add-to-list 'browse-at-remote-remote-type-domains '("192.168.0.202" . "gitlab2"))
+  ;; (add-to-list 'browse-at-remote-remote-type-domains '("mtiisl.cn" . "gitlab3"))
+  ;; (add-to-list 'browse-at-remote-remote-type-domains '("minix.jeanhwea.io" . "gitlab3"))
 
-  (defun browse-at-remote--format-region-url-as-gitlab3
-    (repo-url location filename &optional linestart lineend)
-    "url formatted for gitlab3."
-    (let
-      ((repo-url
-         (jh/re-replace
-           "^\\(https://\\)\\([^/]+\\)\\(.+\\)$"
-           "http://\\2/gitlab\\3" repo-url)))
-      (if (string-match-p "\\.\\(md\\|org\\)$" filename)
-        (browse-at-remote--format-region-url-as-gitlab
-          repo-url location filename)
-        (browse-at-remote--format-region-url-as-gitlab
-          repo-url location filename linestart lineend)))))
+  ;; (defun browse-at-remote--format-commit-url-as-gitlab2 (repo-url commithash)
+  ;;   "commit url formatted for gitlab2."
+  ;;   (let
+  ;;     ((repo-url
+  ;;        (jh/re-replace "^https://" "http://" repo-url)))
+  ;;     (browse-at-remote--format-commit-url-as-gitlab repo-url commithash)))
+
+  ;; (defun browse-at-remote--format-region-url-as-gitlab2
+  ;;   (repo-url location filename &optional linestart lineend)
+  ;;   "url formatted for gitlab2."
+  ;;   (let
+  ;;     ((repo-url
+  ;;        (jh/re-replace "^https://" "http://" repo-url)))
+  ;;     (browse-at-remote--format-region-url-as-gitlab
+  ;;       repo-url location filename linestart lineend)))
+
+  ;; (defun browse-at-remote--format-commit-url-as-gitlab3 (repo-url commithash)
+  ;;   "commit url formatted for gitlab3."
+  ;;   (let
+  ;;     ((repo-url
+  ;;        (jh/re-replace
+  ;;          "^\\(https://\\)\\([^/]+\\)\\(.+\\)$"
+  ;;          "http://\\2/gitlab\\3" repo-url)))
+  ;;     (browse-at-remote--format-commit-url-as-gitlab repo-url commithash)))
+
+  ;; (defun browse-at-remote--format-region-url-as-gitlab3
+  ;;   (repo-url location filename &optional linestart lineend)
+  ;;   "url formatted for gitlab3."
+  ;;   (let
+  ;;     ((repo-url
+  ;;        (jh/re-replace
+  ;;          "^\\(https://\\)\\([^/]+\\)\\(.+\\)$"
+  ;;          "http://\\2/gitlab\\3" repo-url)))
+  ;;     (if (string-match-p "\\.\\(md\\|org\\)$" filename)
+  ;;       (browse-at-remote--format-region-url-as-gitlab
+  ;;         repo-url location filename)
+  ;;       (browse-at-remote--format-region-url-as-gitlab
+  ;;         repo-url location filename linestart lineend))))
+  )
 
 ;; -----------------------------------------------------------------------------
 ;; some helper function with git repository
