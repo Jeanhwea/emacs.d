f2948815f0a3e6d379fc24a6294c12a8b923ffdb
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-09-19 22:13:16 +0800

    update: dired-k has display problem when refresh content

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -5,4 +5,9 @@
       (setq dired-recursive-deletes 'top)
       (define-key dired-mode-map (kbd "C-c w") 'wdired-change-to-wdired-mode)))
 
+;; (when (require 'dired-k)
+;;   (setq dired-k-style 'git)
+;;   (add-hook 'dired-initial-position-hook 'dired-k)
+;;   (add-hook 'dired-after-readin-hook #'dired-k-no-revert))
+
 (provide 'init-dired)
