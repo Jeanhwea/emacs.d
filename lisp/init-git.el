9f6fd14f1ba37379b6e7d81292d9c8164bfc0ded
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-09-20 15:20:27 +0800

    polish: 清理一些无效的 git 补全文档

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -28,49 +28,6 @@
         (cons
           (jh/re-replace "mtiisl.cn" "mtiisl.cn/gitlab" domain)
           (jh/re-replace "mtiisl.cn" "mtiisl.cn/gitlab" url)))))
-
-  ;; (add-to-list 'browse-at-remote-remote-type-domains '("192.168.0.202" . "gitlab2"))
-  ;; (add-to-list 'browse-at-remote-remote-type-domains '("mtiisl.cn" . "gitlab3"))
-  ;; (add-to-list 'browse-at-remote-remote-type-domains '("minix.jeanhwea.io" . "gitlab3"))
-
-  ;; (defun browse-at-remote--format-commit-url-as-gitlab2 (repo-url commithash)
-  ;;   "commit url formatted for gitlab2."
-  ;;   (let
-  ;;     ((repo-url
-  ;;        (jh/re-replace "^https://" "http://" repo-url)))
-  ;;     (browse-at-remote--format-commit-url-as-gitlab repo-url commithash)))
-
-  ;; (defun browse-at-remote--format-region-url-as-gitlab2
-  ;;   (repo-url location filename &optional linestart lineend)
-  ;;   "url formatted for gitlab2."
-  ;;   (let
-  ;;     ((repo-url
-  ;;        (jh/re-replace "^https://" "http://" repo-url)))
-  ;;     (browse-at-remote--format-region-url-as-gitlab
-  ;;       repo-url location filename linestart lineend)))
-
-  ;; (defun browse-at-remote--format-commit-url-as-gitlab3 (repo-url commithash)
-  ;;   "commit url formatted for gitlab3."
-  ;;   (let
-  ;;     ((repo-url
-  ;;        (jh/re-replace
-  ;;          "^\\(https://\\)\\([^/]+\\)\\(.+\\)$"
-  ;;          "http://\\2/gitlab\\3" repo-url)))
-  ;;     (browse-at-remote--format-commit-url-as-gitlab repo-url commithash)))
-
-  ;; (defun browse-at-remote--format-region-url-as-gitlab3
-  ;;   (repo-url location filename &optional linestart lineend)
-  ;;   "url formatted for gitlab3."
-  ;;   (let
-  ;;     ((repo-url
-  ;;        (jh/re-replace
-  ;;          "^\\(https://\\)\\([^/]+\\)\\(.+\\)$"
-  ;;          "http://\\2/gitlab\\3" repo-url)))
-  ;;     (if (string-match-p "\\.\\(md\\|org\\)$" filename)
-  ;;       (browse-at-remote--format-region-url-as-gitlab
-  ;;         repo-url location filename)
-  ;;       (browse-at-remote--format-region-url-as-gitlab
-  ;;         repo-url location filename linestart lineend))))
   )
 
 ;; -----------------------------------------------------------------------------
