53f40aea7f2ae8dc5c948dd891ba3045a7e53c12
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2021-11-04 12:06:48 +0800

    update

--- scripts/install-go-tools.sh
+++ scripts/install-go-tools.sh
@@ -1,6 +1,6 @@
+go install github.com/go-delve/delve/cmd/dlv@latest
 go install github.com/mdempsky/gocode@latest
 go install github.com/rogpeppe/godef@latest
 go install golang.org/x/tools/cmd/godoc@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install golang.org/x/tools/gopls@latest
-go install github.com/go-delve/delve/cmd/dlv@latest
