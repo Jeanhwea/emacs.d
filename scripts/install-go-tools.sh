60766c9dd871645ed2df1363dfebe410378ff412
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2021-11-04 12:06:29 +0800

    add install go delve

--- scripts/install-go-tools.sh
+++ scripts/install-go-tools.sh
@@ -3,3 +3,4 @@ go install github.com/rogpeppe/godef@latest
 go install golang.org/x/tools/cmd/godoc@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install golang.org/x/tools/gopls@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
