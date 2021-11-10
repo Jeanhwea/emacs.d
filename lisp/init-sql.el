b85c03514386706c2014ca76e62c236e348bb181
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2021-11-10 21:40:09 +0800

    update

--- lisp/init-sql.el
+++ lisp/init-sql.el
@@ -33,19 +33,29 @@
 
 ;; http://sqlformat.darold.net/
 ;; https://github.com/darold/pgFormatter
-(defconst pgformat-command
+(defconst pgformat-command-lower
+  (format
+    "%s -p '\\?[0-9]+' -f 1 -u 1 -U 1 -s 2 -w 80 --extra-function \"%s\" -"
+    pgformat-prog pgformat-func-dict)
+  "pg_format command.")
+
+(defconst pgformat-command-upper
   (format
     "%s -p '\\?[0-9]+' -f 2 -u 2 -U 2 -s 2 -w 80 --extra-function \"%s\" -"
     pgformat-prog pgformat-func-dict)
   "pg_format command.")
 
 ;; pip install sqlparse
-(defconst sqlformat-command
+(defconst sqlformat-command-lower
   "sqlformat - -k lower -i lower -s -a --indent_width 2 --wrap_after 80"
   "sqlformat command.")
 
+(defconst sqlformat-command-upper
+  "sqlformat - -k upper -i upper -s -a --indent_width 2 --wrap_after 80"
+  "sqlformat command.")
+
 (defconst sql-format-command
-  (format "%s | %s" sqlformat-command pgformat-command)
+  (format "%s | %s" sqlformat-command-lower pgformat-command-lower)
   "format sql command.")
 
 (defun jh/format-sql-source (&optional file)
@@ -65,7 +75,7 @@
     (if (file-exists-p pgformat-name)
       (let ((default-directory (file-name-directory pgformat-name)))
         (shell-command-on-region beg end sql-format-command nil t))
-      (shell-command-on-region beg end sqlformat-command nil t))
+      (shell-command-on-region beg end sqlformat-command-lower nil t))
     ;; goto previous place
     (when (<= current-point (point-max)) (goto-char current-point))))
 
@@ -89,7 +99,7 @@
     (if (file-exists-p pgformat-name)
       (let ((default-directory (file-name-directory pgformat-name)))
         (shell-command-on-region beg end (concat sql-format-command " -N") nil t))
-      (shell-command-on-region beg end sqlformat-command nil t))
+      (shell-command-on-region beg end sqlformat-command-lower nil t))
     ;; goto previous place
     (when (<= current-point (point-max)) (goto-char current-point))))
 
