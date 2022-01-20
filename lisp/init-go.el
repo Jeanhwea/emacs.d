d281e93b10226644f34442593e1dbb6e4bc7bb67
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-01-20 21:38:17 +0800

    update: 修改 go tag 成 PascalCase

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -48,8 +48,8 @@
       (end-of-line) (hs-toggle-hiding))))
 
 ;; setup for go-tags
-;; (setq go-tag-args '("-transform" "pascalcase"))
-(setq go-tag-args '("-transform" "camelcase"))
+(setq go-tag-args '("-transform" "pascalcase"))
+;; (setq go-tag-args '("-transform" "camelcase"))
 
 (defun jh/run-go-scratch (&optional file)
   "Run go scratch source code."
