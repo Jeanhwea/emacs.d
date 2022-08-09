4c2717633634e9da3fbdd1818c845db4ea43f3ca
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-08-09 18:40:01 +0800

    update: 修改使用 pg_format 来格式化 sql

--- lisp/init-sql.el
+++ lisp/init-sql.el
@@ -35,13 +35,13 @@
 ;; https://github.com/darold/pgFormatter
 (defconst pgformat-command-lower
   (format
-    "%s -p '\\?[0-9]+' -f 1 -u 1 -U 1 -s 2 -w 80 --extra-function \"%s\" -"
+    "%s -p '\\$\\{[_0-9a-zA-Z:]+\\}' -f 1 -u 1 -U 1 -s 2 -w 80 --extra-function \"%s\" -"
     pgformat-prog pgformat-func-dict)
   "pg_format command.")
 
 (defconst pgformat-command-upper
   (format
-    "%s -p '\\?[0-9]+' -f 2 -u 2 -U 2 -s 2 -w 80 --extra-function \"%s\" -"
+    "%s -p '\\$\\{[_0-9a-zA-Z:]+\\}' -f 2 -u 2 -U 2 -s 2 -w 80 --extra-function \"%s\" -"
     pgformat-prog pgformat-func-dict)
   "pg_format command.")
 
@@ -59,7 +59,7 @@
 ;;   "format sql command.")
 
 (defconst sql-format-command
-  (format "%s" sqlformat-command-lower)
+  (format "%s" pgformat-command-lower)
   "format sql command.")
 
 (defun jh/format-sql-source (&optional file)
