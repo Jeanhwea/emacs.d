d01358773d1782747eb91df8886e92108163ff6c
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2023-01-01 12:54:42 +0800

    添加读取默认的 python 解释器

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -68,10 +68,12 @@
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (require 'verb)
 
+  (setq local-python-file "/usr/local/python3/bin/python3")
+
   (add-hook 'org-mode-hook
     #'(lambda ()
         (setq
-          org-babel-python-command "python3"
+          org-babel-python-command (or local-python-file "python3")
           pangu-spacing-real-insert-separtor t)
         (pangu-spacing-mode 1)
         (org-babel-do-load-languages
