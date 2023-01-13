bc6426239b6ad5dc71bcb4c8b6f922e6230cade6
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2023-01-14 02:54:52 +0800

    load lldb on mac

--- lisp/init-debug.el
+++ lisp/init-debug.el
@@ -6,4 +6,7 @@
 (add-hook 'compilation-mode-hook (lambda () (visual-line-mode 1)))
 (add-hook 'compilation-minor-mode-hook (lambda () (visual-line-mode 1)))
 
+;; setup lldb on mac
+(when (jh/mac?) (require 'gud-lldb))
+
 (provide 'init-debug)
