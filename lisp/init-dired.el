93de1c19a34071c84d0065d5c56500eded463985
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-03-01 14:23:40 +0800

    timestamp: 2023-03-01 14:23:40

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -3,6 +3,10 @@
 (add-hook 'after-init-hook
   #'(lambda ()
       (setq dired-recursive-deletes 'top)
+
+      ;; 忽略一些不需要关注的文件
+      (dired-omit-mode 1)
+
       (define-key dired-mode-map (kbd "C-c w") 'wdired-change-to-wdired-mode)))
 
 ;; (when (require 'dired-k)
