e8fab06e94f4bfa7df488502a7a75d7cc5c9a8fd
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-07-27 12:11:17 +0800

    fix asm-mode-hook

--- lisp/init-asm.el
+++ lisp/init-asm.el
@@ -3,7 +3,7 @@
     show-trailing-whitespace t
     indent-tabs-mode t))
 
-(add-hook 'asm-mode-common-hook 'asm-hook-func)
-(add-hook 'nasm-mode-common-hook 'asm-hook-func)
+(add-hook 'asm-mode-hook 'asm-hook-func)
+(add-hook 'nasm-mode-hook 'asm-hook-func)
 
 (provide 'init-asm)
