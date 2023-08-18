c0275f6a03e4fc206f50e3ce8b4c3962e688c0c5
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-08-18 10:08:02 +0800

    add run rust scratch file

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,3 +1,16 @@
 (setq rust-format-on-save t)
 
+(defun jh/run-rust-scratch (&optional file)
+  "Run rust scratch file."
+  (interactive)
+  (let*
+    ((file (or file (buffer-file-name)))
+      (dir (file-name-directory file))
+      (name (file-name-nondirectory file)))
+    (setq cmd0 (format "cd %s" dir))
+    (setq cmd1 (format "rustc %s -o debug.run" name))
+    (setq cmd2 "./debug.run")
+    (setq cmd (concat cmd0 " && " cmd1 " && " cmd2))
+    (emamux:run-command cmd)))
+
 (provide 'init-rust)
--- lisp/init-workflow.el
+++ lisp/init-workflow.el
@@ -661,6 +661,7 @@
     ((eq major-mode 'python-mode) (jh/run-python-scratch))
     ((eq major-mode 'java-mode) (jh/run-java-scratch))
     ((eq major-mode 'sh-mode) (jh/run-shell-scratch))
+    ((eq major-mode 'rust-mode) (jh/run-rust-scratch))
     (t (user-error "Ops: unknown scratch file type."))))
 
 (defun workflow-test-func ()
