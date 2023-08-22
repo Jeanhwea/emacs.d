46c09f0a4d3b840e5cdb2fb8260dad34e2f9c509
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-08-22 20:44:51 +0800

    add window system support for running rust scratch

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -7,15 +7,35 @@
     ((file (or file (buffer-file-name)))
       (dir (file-name-directory file))
       (name (file-name-nondirectory file)))
-
     (save-buffer)
+    (if (not window-system)
+      (jh/run-rust-scratch-in-tmux dir name)
+      (jh/run-rust-scratch-gui dir name))))
 
-    (emamux:run-command (format "cd %s" dir))
-    (emamux:run-command "clear")
+(defun jh/run-rust-scratch-gui (dir name)
+  (let
+    ((sbufname "*rust-scratch-buffer*") (default-directory dir))
 
     (setq cmd1 (format "rustc %s -o debug.run" name))
     (setq cmd2 "./debug.run")
     (setq cmd (concat cmd1 " && " cmd2))
-    (emamux:run-command cmd)))
+
+    (progn
+      (if (get-buffer sbufname)
+        (setq sbuf (get-buffer sbufname))
+        (setq sbuf (generate-new-buffer sbufname)))
+      (shell-command cmd sbuf sbuf)
+      (display-buffer sbuf)
+      (message (format "Run %s" name)))))
+
+(defun jh/run-rust-scratch-in-tmux (dir name)
+  "Run rust scratch in tmux."
+  (emamux:run-command (format "cd %s" dir))
+  (emamux:run-command "clear")
+  (setq cmd1 (format "rustc %s -o debug.run" name))
+  (setq cmd2 "./debug.run")
+  (setq cmd (concat cmd1 " && " cmd2))
+  (emamux:run-command cmd)
+  (message (format "Run %s" name)))
 
 (provide 'init-rust)
