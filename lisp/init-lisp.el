2822b5ada67cec9ff1ab28b1739bc4435935279e
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-10-30 10:41:39 +0800

    fix

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -26,6 +26,6 @@
       (company-fuzzy-mode 1)
       (rainbow-delimiters-mode)))
 
-(add-to-list 'auto-mode-alist '("\\.dir-locals.el\\'" . lisp-mode))
+(add-to-list 'auto-mode-alist '(".dir-locals.el\\'" . emacs-lisp-mode))
 
 (provide 'init-lisp)
