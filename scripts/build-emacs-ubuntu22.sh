69eb96fa3a85ec23d171f5ae98b30a67fa14df65
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-06-02 12:27:37 +0800

    feat: 添加源码构建脚本

---
+++ scripts/build-emacs-ubuntu22.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+EBASE=`cd $(dirname $0); cd ..; pwd`
+EMVER="29.3"
+EMSRC="/opt"
+
+sudo apt-get update && apt-get upgrade
+sudo apt-get install build-essential libncurses-dev autoconf make gcc \
+     texinfo libgtk-3-dev libxpm-dev libjpeg-dev libgif-dev libtiff5-dev \
+     libncurses5-dev libxml2-dev libgnutls28-dev
+
+if [ ! -d $EMSRC ]; then 
+  echo "set env '$EMSRC' please, $EMSRC not found"
+  exit 1
+fi
+
+cd $EMSRC
+wget -c https://mirrors.tuna.tsinghua.edu.cn/gnu/emacs/emacs-${EMVER}.tar.xz
+tar xjvf emacs-${EMVER}.tar.xz
+cd emacs-${EMVER}
+
+./autogen.sh
+./configure --prefix=/opt/emacs
+
+make -j $(nproc)
+make docs
+
+sudo make install && make install-doc
+
